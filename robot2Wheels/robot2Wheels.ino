901c7f561dbdff5f6ab19315636e6c71f1f7f6c2
MisaZhu <misa.zhu@gmail.com>
2013-03-08 00:14:36 +0800

    balance with gyroscope

--- robot2Wheels/robot2Wheels.ino
+++ robot2Wheels/robot2Wheels.ino
@@ -1,24 +1,53 @@
 
 #include "Motor.h"
+#include "GyroMPU6050.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include <Wire.h>
 
+GyroMPU6050 gyro;
+float stable = 0.0;
 
-void setup() {
+Motor m1(6, 7), m2(4, 5);
   
+  
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  gyro.init();
+  
+  digitalWrite(13, 1);
+  
+  delay(2000);
+  
+  EulerT el;
+  while(true) {
+    if(gyro.readEuler(el))
+      break;
+  }
+  stable = el.theta;
+  digitalWrite(13, 0);
 }
 
 void loop() {
-  Motor m1(2, 3), m2(4, 5);
-  m1.back(200);
-  m2.back(200);
-    
-    
-  delay(1000);
-    
-// m.brake();
- delay(200);
- m1.forward(200);
- m2.forward(200);
-    
- delay(1000);
   
+  EulerT el;
+  while(!gyro.readEuler(el));
+  
+  Serial.print(el.theta);
+  Serial.print("\t -->\t");
+  Serial.println(stable);
+  
+  if((el.theta) < stable) {
+     m1.forward(40);
+     m2.forward(40);
+  }
+  else if((el.theta) > stable) {
+     m1.back(40);
+     m2.back(40);
+  }
+  //delay(10);
+  //m1.brake();
+ // m2.brake();
+  //delay(10);
 }
