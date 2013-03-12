66f7e5bd085daaa98ef54056d9aa125caa7eaccf
MisaZhu <misa.zhu@gmail.com>
2013-03-12 23:44:02 +0800

    PID Controll

--- robot2Wheels/robot2Wheels.ino
+++ robot2Wheels/robot2Wheels.ino
@@ -4,50 +4,198 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include <Wire.h>
+#include <PID_v1.h>
+#include <math.h>
+#include <String.h>
+#include <LCD5110_Graph.h>
+#include <StringFormat.h>
+#include <PID_AutoTune_v0.h>
 
 GyroMPU6050 gyro;
-float stable = 0.0;
 
-Motor m1(6, 7), m2(4, 5);
-  
-  
+double stable =0.0 , input, output;
+double kp=2,ki=0.5,kd=2;
+double aTuneStep=100, aTuneNoise=10, aTuneStartValue=255;
+unsigned int aTuneLookBack=20;
+byte ATuneModeRemember=2;
+unsigned long debugTime = 0;
+
+Motor m1(6, 7), m2(5, 4);
+
+PID pid( &input, &output, &stable, kp, ki, kd,  REVERSE);
+PID_ATune aTune(&input, &output);
+
+LCD5110 myGLCD(8,9,10,11,12);
+
+boolean tuning = true;
+
+extern uint8_t SmallFont[];
+
+
+void changeAutoTune()
+{
+  if(!tuning)
+  {
+    //Set the output to the desired starting frequency.
+    output=aTuneStartValue;
+    aTune.SetNoiseBand(aTuneNoise);
+    aTune.SetOutputStep(aTuneStep);
+    aTune.SetLookbackSec((int)aTuneLookBack);
+    AutoTuneHelper(true);
+    tuning = true;
+  }
+  else
+  { //cancel autotune
+    aTune.Cancel();
+    tuning = false;
+    AutoTuneHelper(false);
+  }
+}
+
+void AutoTuneHelper(boolean start)
+{
+  if(start)
+    ATuneModeRemember = pid.GetMode();
+  else
+    pid.SetMode(ATuneModeRemember);
+}
+
+
+
+char* f2s(float f) {
+  static char ret[25];
+  return StringFormat::fromFloat(ret, f, 5);
+}
+
+void display(char* s, int line = 0) {
+  myGLCD.clrScr();
+  myGLCD.print(s, 0, line * 16);
+  myGLCD.update();
+}
+
+
 void setup() {
+  myGLCD.InitLCD();
+  myGLCD.setFont(SmallFont);
+
   Serial.begin(9600);
+
+  //Serial.println("Initializing......");
+  display("Initializing......");
   Wire.begin();
   gyro.init();
-  
-  digitalWrite(13, 1);
-  
-  delay(2000);
-  
-  EulerT el;
-  while(true) {
-    if(gyro.readEuler(el))
-      break;
+
+  pid.SetMode(AUTOMATIC);
+  pid.SetOutputLimits(0, 500);
+
+  if(tuning) {
+    tuning=false;
+    changeAutoTune();
+    tuning=true;
   }
-  stable = el.theta;
-  digitalWrite(13, 0);
+
+  //Serial.println("Ready!");
+  display("Ready......");
+}
+
+
+void drive(double output) {
+  bool forward = true;
+
+  if(output < 0)
+    forward = false;
+  output = abs(output);
+
+  if(output < 3) {
+    m1.brake();
+    m2.brake();
+    return;
+  }
+
+  int powerL = min(255, output + 45);
+  int powerR = powerL;
+
+  if(forward) {
+    m1.forward(powerL);
+    m2.forward(powerR);
+    //     Serial.print(powerL);
+    //     Serial.println("\tforward");
+  }
+  else {
+    m1.back(powerL);
+    m2.back(powerR);
+    //     Serial.print(powerL);
+    //     Serial.println("\tback");
+  }
+  // delay(2); 
 }
 
 void loop() {
-  
-  EulerT el;
-  while(!gyro.readEuler(el));
-  
-  Serial.print(el.theta);
-  Serial.print("\t -->\t");
-  Serial.println(stable);
-  
-  if((el.theta) < stable) {
-     m1.forward(40);
-     m2.forward(40);
+
+  Motion6T m6;
+  while(!gyro.readMotion6(m6));
+    
+
+  input = atan2(m6.ay, m6.az) * 180 / 3.1415926 - 5.0 ;
+
+  if(input < -45 || input > 45) {
+    //drive(0);
+    //Serial.println(input);
+    //Serial.println("\tfall down.");
+    //delay(1000);
+    return;
   }
-  else if((el.theta) > stable) {
-     m1.back(40);
-     m2.back(40);
+
+  if(tuning)
+  {
+    byte val = (aTune.Runtime());
+    if (val!=0)
+    {
+      tuning = false;
+    }
+
+    if(!tuning)
+    { //we're done, set the tuning parameters
+      kp = aTune.GetKp();
+      ki = aTune.GetKi();
+      kd = aTune.GetKd();
+      pid.SetTunings(kp,ki,kd);
+      AutoTuneHelper(false);
+      Serial.println("done");
+      display("Tuned!", 0);
+    }
   }
-  //delay(10);
-  //m1.brake();
- // m2.brake();
-  //delay(10);
+  else {
+    pid.Compute();
+  }
+
+  if(millis() > debugTime) {
+    Serial.print(kp);
+    Serial.print("\t -->\t");
+    Serial.print(ki);
+    Serial.print("\t -->\t");
+    Serial.print(kd);
+    Serial.print("\t     ||     \t");
+    Serial.print(input);
+    Serial.print("\t -->\t");
+    Serial.print(stable);
+    Serial.print("\t -->\t");
+    Serial.print(output);
+    Serial.print(", ");
+    Serial.print(output - 255);
+    if(tuning)
+      Serial.println("\ttuning.");
+    else
+      Serial.println("\tdone.");
+
+   // debugTime += 100;
+  }
+
+  drive(output - 255);
+  
+  delay(30);
+  drive(0);
 }
+
+
+
