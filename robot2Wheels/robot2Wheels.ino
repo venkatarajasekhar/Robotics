1c12ed65323dcc922b6f23c7a395fca673e671c7
MisaZhu <misa.zhu@gmail.com>
2013-03-17 15:07:06 +0800

    try MMA7361 gyro

--- robot2Wheels/robot2Wheels.ino
+++ robot2Wheels/robot2Wheels.ino
@@ -1,64 +1,69 @@
 
 #include "Motor.h"
-#include "GyroMPU6050.h"
-#include "I2Cdev.h"
-#include "MPU6050.h"
-#include <Wire.h>
 #include <PID_v1.h>
 #include <math.h>
 #include <String.h>
 #include <LCD5110_Graph.h>
 #include <StringFormat.h>
-#include <PID_AutoTune_v0.h>
 
-GyroMPU6050 gyro;
 
 double stable =0.0 , input, output;
-double kp=1,ki=0.5,kd=9;
-double aTuneStep=100, aTuneNoise=10, aTuneStartValue=255;
-unsigned int aTuneLookBack=20;
-byte ATuneModeRemember=2;
+double targetPos;
 unsigned long debugTime = 0;
 
-Motor m1(6, 7), m2(5, 4);
+//#define GYRO_MPU6050
 
-PID pid( &input, &output, &stable, kp, ki, kd,  REVERSE);
-PID_ATune aTune(&input, &output);
+#ifdef GYRO_MPU6050
+
+#include "GyroMPU6050.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include <Wire.h>
+GyroMPU6050 gyro;
+
+double getTargetPos() {
+  return 0.0;
+}
+
+double getInput() {
+  Motion6T m6;
+  gyro.readMotion6(m6);
+  return (atan2(m6.ay, m6.az) * 180 / 3.1415926  - targetPos);
+}
+
+#else
+
+double getAngle() {
+  double x = analogRead(0);
+  double z = analogRead(2);
+  return (atan2(x, z) * 180 / 3.1415926);
+}
+
+double getTargetPos() {
+  digitalWrite(13, 0);
+  delay(2000);
+  double ret =  getAngle();
+  digitalWrite(13, 1);
+  return ret;
+}
+
+
+
+double getInput() {
+  double pos = getAngle();
+  return (pos  - targetPos);
+}
+
+#endif
+
+
+
+Motor m1(6, 7), m2(4, 5);
+
+PID pid( &input, &output, &stable,  2, 0, 0,  REVERSE);
 
 LCD5110 myGLCD(8,9,10,11,12);
-
-boolean tuning = false;
-
 extern uint8_t SmallFont[];
-
-
-void changeAutoTune()
-{
-  if(!tuning)
-  {
-    //Set the output to the desired starting frequency.
-    output=aTuneStartValue;
-    aTune.SetNoiseBand(aTuneNoise);
-    aTune.SetOutputStep(aTuneStep);
-    aTune.SetLookbackSec((int)aTuneLookBack);
-    AutoTuneHelper(true);
-    tuning = true;
-  }
-  else
-  { //cancel autotune
-    aTune.Cancel();
-    tuning = false;
-    AutoTuneHelper(false);
-  }
-}
-
-void AutoTuneHelper(boolean start)
-{
-  if(start)
-    ATuneModeRemember = pid.GetMode();
-  else
-    pid.SetMode(ATuneModeRemember);
-}
 
 void display(char* s, int line = 0) {
   myGLCD.clrScr();
@@ -75,17 +80,11 @@ void setup() {
 
   Serial.println("Initializing......");
   display("Initializing......");
-  Wire.begin();
-  gyro.init();
+  targetPos = getTargetPos();
 
   pid.SetMode(AUTOMATIC);
   pid.SetOutputLimits(-255, 255);
-
-  if(tuning) {
-    tuning=false;
-    changeAutoTune();
-    tuning=true;
-  }
+  pid.SetSampleTime(60);
 
   Serial.println("Ready!");
   display("Ready......");
@@ -105,7 +104,7 @@ void drive(double output) {
     return;
   }
 
-  int powerL = min(255, output + 45);
+  int powerL = min(255, output + 100);
   int powerR = powerL;
 
   if(forward) {
@@ -124,70 +123,31 @@ void drive(double output) {
 }
 
 void loop() {
+  input = getInput();
 
-  Motion6T m6;
-  gyro.readMotion6(m6);
-    
-
-  input = atan2(m6.ay, m6.az) * 180 / 3.1415926 - 2.0 ;
-
-  if(input < -45 || input > 45) {
-    //drive(0);
+  /*if(input < -45 || input > 45) {
+    drive(0);
     //Serial.println(input);
     //Serial.println("\tfall down.");
     //delay(1000);
     return;
-  }
+  }*/
 
-  if(tuning)
-  {
-    byte val = (aTune.Runtime());
-    if (val!=0)
-    {
-      tuning = false;
-    }
+  pid.Compute();
 
-    if(!tuning)
-    { //we're done, set the tuning parameters
-      kp = aTune.GetKp();
-      ki = aTune.GetKi();
-      kd = aTune.GetKd();
-      pid.SetTunings(kp,ki,kd);
-      AutoTuneHelper(false);
-      Serial.println("done");
-      display("Tuned!", 0);
-    }
-  }
-  else {
-    pid.Compute();
-  }
-
-  if(millis() > debugTime) {
-    Serial.print(kp);
-    Serial.print("\t -->\t");
-    Serial.print(ki);
-    Serial.print("\t -->\t");
-    Serial.print(kd);
-    Serial.print("\t     ||     \t");
+ if(millis() > debugTime) {
     Serial.print(input);
     Serial.print("\t -->\t");
     Serial.print(stable);
     Serial.print("\t -->\t");
-    Serial.print(output);
-    //Serial.print(", ");
-    //Serial.print(output - 255);
-    if(tuning)
-      Serial.println("\ttuning.");
-    else
-      Serial.println("\tdone.");
+    Serial.println(output);
 
     debugTime += 100;
   }
 
- // drive(output - 255);
-  drive(-output);
-  delay(10);
-  drive(0);
+  //drive(-output);
+  delay(20);
+  //drive(0);
 }
 
 
