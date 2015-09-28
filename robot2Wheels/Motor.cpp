a429d1a935efde5132bd23818783888a2f1b8f78
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-09-28 11:52:33 +0530

    Motor.cpp

--- robot2Wheels/Motor.cpp
+++ robot2Wheels/Motor.cpp
@@ -2,24 +2,56 @@
 #include <Arduino.h>
 
 Motor::Motor(int pin1, int pin2) {
+  try{
   this->pin1 = pin1;
-  this->pin2 = pin2;    
+  }catch(...){
+    
+  }
+  try{
+    this->pin2 = pin2;    
+  }catch(...){
+    
+  }
 }
 
 void Motor::forward(int speed) {
+  try{
   analogWrite(pin1, speed);
-  analogWrite(pin2, 0);
+  }catch(...){
+    
+  }
+  try{
+    analogWrite(pin2, 0);
+  }catch(...){
+    
+  }
 }
 
 
 void Motor::back(int speed) {
+  try{
   analogWrite(pin2, speed);
-  analogWrite(pin1, 0);
+  }catch(...){
+    
+  }
+  try{
+    analogWrite(pin1, 0);
+  }catch(...){
+    
+  }
 }
 
 void Motor::brake() {
+  try{
   analogWrite(pin2, 255);
-  analogWrite(pin1, 255);
+  }catch(...){
+    
+  }
+  try{
+    analogWrite(pin1, 255);
+  }catch(...){
+    
+  }
 }
 
 
